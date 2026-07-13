563ee3971955539239e6e945aea234a864060b19
47n8c866wj-creator <47n8c866wj@privaterelay.appleid.com>
2026-07-13 21:05:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const player = document.getElementById("player");
+const enemy = document.getElementById("enemy");
 
+let playerX = 135;
+let enemyY = -100;
+let enemyX = Math.floor(Math.random() * 3) * 90 + 45;
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft" && playerX > 45) {
+        playerX -= 90;
+    }
+
+    if (e.key === "ArrowRight" && playerX < 225) {
+        playerX += 90;
+    }
+
+    player.style.left = playerX + "px";
+});
+
+function gameLoop() {
+    enemyY += 5;
+
+    if (enemyY > 600) {
+        enemyY = -100;
+        enemyX = Math.floor(Math.random() * 3) * 90 + 45;
+    }
+
+    enemy.style.top = enemyY + "px";
+    enemy.style.left = enemyX + "px";
+
+    if (
+        enemyY > 470 &&
+        enemyY < 560 &&
+        enemyX === playerX
+    ) {
+        alert("💥 Game Over!");
+        enemyY = -100;
+    }
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
